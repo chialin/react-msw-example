c6bdd319c5c2e3d8e3ac83a95ec0b6f8a2a2f01f
chialin <chialin.shr@gmail.com>
2024-10-02 20:44:24 +0800

    Fixed Uncaught (in promise) SyntaxError: Unexpected token '<', "<!doctype "... is not valid JSON

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// https://mswjs.io/docs/integrations/browser#conditionally-enable-mocking
+async function enableMocking() {
+  const { worker } = await import('./mocks/browser');
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+if (process.env.NODE_ENV === 'development') {
+  await enableMocking();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
